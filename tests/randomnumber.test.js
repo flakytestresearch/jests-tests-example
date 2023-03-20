e909c2f3e411330f8642fe8837be7c3948010b83
sorattorafa <rampimsoratto@gmail.com>
2023-03-19 21:09:44 -0300

    update test and action

--- tests/randomnumber.test.js
+++ tests/randomnumber.test.js
@@ -2,9 +2,9 @@ function get_random_number(a) {
     return Math.random() * a;
 }
 
-test('Get random from 0 to 100', () => {
-  const randomNumber = get_random_number(100);
-  const greatherNumber = 95;
+test('Get random from 0 to 10', () => {
+  const randomNumber = get_random_number(10);
+  const greatherNumber = 5;
   console.log(randomNumber, greatherNumber)
   expect(randomNumber).toBeLessThan(greatherNumber);
 });
