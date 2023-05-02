d079703e8e4cd015821be990d62ef4e5dacc69e3
Rafael Rampim Soratto <rafael.soratto@appmoove.com.br>
2023-05-02 10:35:00 -0300

    add flaky tests

--- tests/objectserialize.test.js
+++ tests/objectserialize.test.js
@@ -1,18 +1,24 @@
-function get_serialized_object() {
-    return {
-      'name': 'Rafael',
-      'age': 24,
-      'work': 'Flaky tests',
-      'creagted_at': Date.now()
-    }
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-test('Object serialize test', () => {
+function get_serialized_object() {
+  const formattedDateTime = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
+  return {
+    name: "Rafael",
+    age: 24,
+    work: "Flaky tests",
+    creagted_at: formattedDateTime,
+  };
+}
+
+test("Object serialize test", async () => {
   const person = get_serialized_object();
+  // await delay(1000);
   expect(person).toMatchObject(get_serialized_object());
 });
 
-test('Object serialize test', () => {
+test("Object serialize test", () => {
   const person = get_serialized_object();
   expect(person).toMatchObject(person);
 });
