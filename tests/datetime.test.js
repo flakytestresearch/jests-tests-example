4c5485c3adc4567d210a7c8b3a13b43092509236
Rafael Rampim Soratto <rafael.soratto@appmoove.com.br>
2023-05-04 10:08:47 -0300

    add datetime flaky tests

---
+++ tests/datetime.test.js
@@ -0,0 +1,9 @@
+const getCurrentSeconds = () => new Date().getSeconds()
+
+test('flaky test: current second is greater than the previous run', async () => {
+  const second1 = getCurrentSeconds();
+  await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
+  const second2 = getCurrentSeconds();
+
+  expect(second2).toBeGreaterThan(second1);
+});
